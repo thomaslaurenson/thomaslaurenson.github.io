cd106a00dd5e543070c08942896487557291560c
thomaslaurenson <thomas@thomaslaurenson.com>
2018-07-01 12:12:18 +1200

    Fixed footer display error for icons with tooltip. Changed blog index to not show thumbnail in 'mobile' display mode.

--- jekyll-serve.sh
+++ jekyll-serve.sh
@@ -6,4 +6,4 @@ rm -rf _site
 
 bundle exec jekyll serve --host 0.0.0.0
 #bundle exec jekyll serve --incremental --host 0.0.0.0
-#bundle exec jekyll serve --drafts --incremental --host 0.0.0.0
+#bundle exec jekyll serve --drafts --host 0.0.0.0
