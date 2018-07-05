f7091814e14ed65f10515de48d79c52aa27fa466
thomaslaurenson <thomas@thomaslaurenson.com>
2018-07-05 15:46:06 +1200

    Slight modification of jekyll-serve.sh to default to current build requirements.

--- jekyll-serve.sh
+++ jekyll-serve.sh
@@ -4,6 +4,9 @@ set -e
 
 rm -rf _site
 
-bundle exec jekyll serve --host 0.0.0.0
+bundle exec jekyll server --future --incremental --host 0.0.0.0
+
+#bundle exec jekyll server --host 0.0.0.0
+#bundle exec jekyll serve --future --host 0.0.0.0
 #bundle exec jekyll serve --incremental --host 0.0.0.0
 #bundle exec jekyll serve --drafts --host 0.0.0.0
