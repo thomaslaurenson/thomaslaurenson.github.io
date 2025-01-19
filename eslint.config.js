b32dd085d1f591a3bf6ee72bc0cfd32aec3760ad
thomaslaurenson <thomas@thomaslaurenson.com>
2025-01-20 07:55:50 +1300

    Updated packages and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,8 +4,21 @@ import pluginReact from "eslint-plugin-react";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    settings: {
+    react: {
+      version: "detect",
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"]
+  },
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
