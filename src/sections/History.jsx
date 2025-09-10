0ffa23b7d67d9076767c8ed008ff75ad52aaee99
thomaslaurenson <thomas@thomaslaurenson.com>
2025-09-11 08:25:58 +1200

    Moved constants to dedicated file

--- src/sections/History.jsx
+++ src/sections/History.jsx
@@ -5,52 +5,9 @@ import {
   Typography,
 } from '@mui/material';
 import HistoryCard from '../components/HistoryCard';
+import { employers } from '../data/constants';
 
 const History = () => {
-  const employers = [
-    {
-      'company': 'University of Auckland',
-      'position': 'Senior Security Engineer',
-      'avatar': 'uoa',
-      'dates': '2024 - present',
-    },
-    {
-      'company': 'Cogo',
-      'position': 'Senior Security Engineer',
-      'avatar': 'cogo',
-      'dates': '2022 - 2023',
-    },
-    {
-      'company': 'Quantum Security',
-      'position': 'Senior Security Consultant (Pentester)',
-      'avatar': 'quantum',
-      'dates': '2021 - 2022',
-    },
-    {
-      'company': 'Xero',
-      'position': 'Senior Cybersecurity Education Analyst',
-      'avatar': 'xero',
-      'dates': '2020 - 2021',
-    },
-    {
-      'company': 'Otago Polytechnic',
-      'position': 'Lecturer',
-      'avatar': 'op',
-      'dates': '2017 - 2019',
-    },
-    {
-      'company': 'University of Otago',
-      'position': 'PhD Candidate/Lab Demonstrator',
-      'avatar': 'uoo',
-      'dates': '2013 - 2017',
-    },
-    {
-      'company': 'Auckland University of Technology',
-      'position': 'Masters Candidate/Teaching Assistant',
-      'avatar': 'aut',
-      'dates': '2010 - 2013',
-    }];
-
   return (
     <Box pt={6} pb={4} id='history'>
       <Box pb={6}>
